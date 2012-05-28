de24b3bccb328f628682d0729cfbaa1d1e9073e6
Geoff White <geoffw@durganetworks.com>
2012-05-28 13:30:01 -0700

    Additional Baseclass method definition to the Hypervisor baseclass, clearer definition of networking in the hvdef.yaml and vmdef.yaml files

--- src/hypervisor.py
+++ src/hypervisor.py
@@ -44,6 +44,9 @@ class Hypervisor:
         session.expect([pexpect.TIMEOUT, "~ #"], timeout=30)
 
     def ctl_session(self,orig_prompt='[#$] '):
+        ''' For Hypervisors that have CLI console that you can ssh into (most)
+        Open up an ssh control session, that you can send VLI commands too
+        '''
         self.orig_prompt = orig_prompt
         if len(self.hvdict["session_list"]) == self.hvdict["MaxCtlsessions"]:
             raise HVCtlchannelError("Maximum number of control sessions has been reached")
@@ -67,15 +70,6 @@ class Hypervisor:
         self.hvdict["session_list"].append(ctnl)
         return ctnl
 
-    def restart(self, *args, **kwargs):
-        raise HVUnimplemented("restart")
-
-    def maintenance(self, *args, **kwargs):
-        raise HVUnimplemented("maintenance")
-
-    def shutdown(self, *args, **kwargs):
-        raise HVUnimplemented("shutdown")
-
     def sendcmd(self,chan,cmd,expect_string=None,timeout=3, op=None):
         self._consume_prompt(chan)
         op = op if op else self.orig_prompt
@@ -83,6 +77,76 @@ class Hypervisor:
         chan.sendline(cmd)
         retval = chan.expect([pexpect.TIMEOUT, op, expect_string], timeout=timeout)
         return (retval, chan.before, chan.after)
+
+    # The following Methods manipulate global state on the Hypervisor, these operations can
+    # cause side effects that could effect current and future VMs that are running.
+
+    def restart(self, *args, **kwargs):
+        ''' restart/reboot or reset the Hypervisor, this operation is vendor specific
+        '''
+        raise HVUnimplemented("restart")
+
+    def maintenance_enter(self, *args, **kwargs):
+        ''' put the Hypervisor into maintenance mode (vmware), rescue mode (openstack)
+        this operation is vendor specific, generally VMs are suspended, evacuated or
+        powered off
+        '''
+        raise HVUnimplemented("maintenance_enter")
+
+    def maintenance_exit(self, *args, **kwargs):
+        ''' exit maintenance mode and restore the Hypervisor to normal operation
+        this operation is vendor specific
+        '''
+        raise HVUnimplemented("maintenance_exit")
+
+    def shutdown(self, *args, **kwargs):
+        ''' shutdown the Hypervisor, requiring "manual" restart, this operation is 
+        vendor specific
+        '''
+        raise HVUnimplemented("shutdown")
+
+    def define_network(self, *args, **kwargs):
+        ''' define logical networking on the hypervisor, this could include vswitches,
+        and portgroups (vmware), bridges, taps, etc. this operation is vendor specific
+        '''
+        raise HVUnimplemented("define_network")
+
+    def mount_object_store(self, *args, **kwargs):
+        ''' binds an "object storwe" to a hypervisor for purposes of providing access to images
+        for future vm_register and other vm related operations, this operation is vendor
+        specific
+        '''
+        raise HVUnimplemented("mount_object_store")
+
+    def register_vm(self, *args, **kwargs):
+        ''' instantiate a runable VM from an image found on the mounted object store, make the
+        VM runnable, but don't start it. This should return an object ref that can be used in
+        the subsequent vm_xx calls.
+        '''
+        raise HVUnimplemented("register_vm")
+
+    # The following methods perform VM related operations on the specific Hypervisor, only the
+    # VM that it is applied to should be affected
+
+
+    def vm_start(self, *args, **kwargs):
+        raise HVUnimplemented("vm_start")
+
+    def vm_stop(selfself,*args, **kwargs):
+        raise HVUnimplementer("vm_stop")
+
+    def vm_restart(self, *args, **kwargs):
+        raise HVUnimplemented("vm_restart")
+
+    def vm_suspend(self, *args, **kwargs):
+        raise HVUnimplemented("vm_suspend")
+
+    def vm_query_ip(selfself,*args, **kwargs):
+        raise HVUnimplementer("vm_query_ip")
+
+    def vm_destroy(selfself,*args, **kwargs):
+        raise HVUnimplementer("vm_destroy")
+
 
 # internal functions & classes
 
